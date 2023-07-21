5b74059105afdf3a70e6867d0765ec43c4cc3a50
Dmitriy <pd2018@yandex.ru>
2023-07-21 21:48:51 +0300

    Changed interface Book

--- class_02/Book.java
+++ class_02/Book.java
@@ -4,5 +4,19 @@ package class_02;
  * Интерфейс описывает сущность Книга.
  */
 public interface Book {
-    void setPublictionType();
+    void setPublictionType(PublicationType type);
+
+    PublicationType getPublicationType();
+
+    void setBookName(String name);
+
+    String getBookName();
+
+    void setBookAuthor(String author);
+
+    String getBookAuthor();
+
+    void setPagesNumber(Integer number);
+
+    Integer getPagesNumber();
 }
