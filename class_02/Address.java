8754a8b3b316086025a2e02ce8971565fdb8edc8
Dmitriy <pd2018@yandex.ru>
2023-07-21 21:03:31 +0300

    Added interface Address

---
+++ class_02/Address.java
@@ -0,0 +1,15 @@
+package class_02;
+
+public interface Address {
+    void setCity(String city);
+    
+    String getCity();
+
+    void setStreet(String street);
+
+    String getStreet();
+
+    void setBuildingNumber(Integer number);
+
+    Integer getBuildingNumber();
+}
