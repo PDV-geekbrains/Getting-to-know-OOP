ff746de96569c437af41ea066788df4a60595892
Dmitriy <pd2018@yandex.ru>
2023-07-21 22:23:34 +0300

    Interface Book: added constatnts.

--- class_02/Book.java
+++ class_02/Book.java
@@ -4,6 +4,11 @@ package class_02;
  * Интерфейс описывает сущность Книга.
  */
 public interface Book {
+    // Жёсткая обложка.
+    int HARDCOVER = 1;
+    // Мягкая облжка.
+    int SOFTCOVER = 0;
+    
     void setPublictionType(PublicationType type);
 
     PublicationType getPublicationType();
