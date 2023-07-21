3d082f61656773c4db8a1568fcfe01166be9c54a
Dmitriy <pd2018@yandex.ru>
2023-07-21 20:53:29 +0300

    Added interface Library

---
+++ class_02/Library.java
@@ -0,0 +1,10 @@
+package class_02;
+
+public interface Library {
+    String getName();
+    void setName(String name);
+    String getAddress();
+    void setAddress(String address)
+    String getRegularHours();
+    void setRegularHours(String getRegularHours);
+}
