87e8e68e8b375b8967de840daa80b714558007b1
Dmitriy <pd2018@yandex.ru>
2023-07-21 21:25:43 +0300

    Interface Address: added interface caption.

--- class_02/Address.java
+++ class_02/Address.java
@@ -1,8 +1,11 @@
 package class_02;
 
+/**
+ * Интерфейс определяет адрес библиотеки.
+ */
 public interface Address {
     void setCity(String city);
-    
+
     String getCity();
 
     void setStreet(String street);
