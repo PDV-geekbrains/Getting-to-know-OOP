fe8c62bd4bd5a363cbff96633676f1d3819af911
Dmitriy <pd2018@yandex.ru>
2023-07-23 22:17:07 +0300

    Created class class_03_01/MonthlyMagazine.

---
+++ class_03_01/MonthlyMagazine.java
@@ -0,0 +1,37 @@
+package class_03_01;
+
+import class_02.PublicationType;
+
+public class MonthlyMagazine implements IMagazine {
+
+    @Override
+    public PublicationType getPublicationType() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPublicationType'");
+    }
+
+    @Override
+    public String getMagazineName() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getMagazineName'");
+    }
+
+    @Override
+    public void setMagazineName(String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setMagazineName'");
+    }
+
+    @Override
+    public Integer getPagesNumber() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPagesNumber'");
+    }
+
+    @Override
+    public void setPagesNumber(Integer number) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setPagesNumber'");
+    }
+    
+}
