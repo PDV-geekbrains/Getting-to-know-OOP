87ebfbd032e1737f902c0bdf703fdceddbaabed7
Dmitriy <pd2018@yandex.ru>
2023-07-28 22:03:39 +0300

    Added class_04_02/program

---
+++ class_04_02/program.java
@@ -0,0 +1,32 @@
+package class_04_02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class program {
+    /**  Вывод на консоль элементов массивов типов Integer и String.
+     */
+    public static void main(String[] args) {
+        ArrayList<Integer> listOfIntegers = getArrayOfIntegers();
+        ArrayList<String> listOfStrings = getArrayOfStrings();
+        
+        printArray(listOfIntegers);
+        printArray(listOfStrings);
+    }
+    
+    /** Обобщённый метод выводит на консоль элементы входного массива. */
+    private static <T> void printArray(Iterable<T> array) {
+        for (T item : array)
+            System.out.println(item);
+    }
+    
+    /** Метод возвращает массив случайных элеметов типа Integer. */
+    private static ArrayList<Integer> getArrayOfIntegers() {
+        return new ArrayList<Integer>(List.of(1, 2, 3));
+    }
+
+    /** Метод возвращает массив случайных элеметов типа String. */
+    private static ArrayList<String> getArrayOfStrings() {
+        return new ArrayList<String>(List.of("one", "two", "three"));
+    }
+}
