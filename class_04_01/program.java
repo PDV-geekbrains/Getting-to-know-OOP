7a6b40a41c7d1320485d61488f7b95c0357b35e7
Dmitriy <pd2018@yandex.ru>
2023-07-28 21:01:51 +0300

    Added class_04_01.program

---
+++ class_04_01/program.java
@@ -0,0 +1,20 @@
+package class_04_01;
+
+public class program {
+    /** 
+     * Последовательный ввод на консоль данных
+     * разных типов.
+     */
+    public static void main(String[] args) {
+        printElement(123);
+        printElement("String");
+        printElement(123.456);
+    }
+    
+    /**
+     * Обобщённый метод выводит на консоль входной параметр.
+     */
+    private static <T> void printElement(T parameter){
+        System.out.println(parameter);
+    }
+}
