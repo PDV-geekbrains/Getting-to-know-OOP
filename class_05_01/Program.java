73fb7da71863ab734d6021649da060ddc1de4e7d
Dmitriy <pd2018@yandex.ru>
2023-08-02 22:14:07 +0300

    Added comments to class_05_01.program.

--- class_05_01/Program.java
+++ class_05_01/Program.java
@@ -2,12 +2,17 @@ package class_05_01;
 
 import java.util.Scanner;;
 
+/** Программа реализует калькулятор с четырьмя
+ * арифметическими действиями.
+ */
 public class Program {
     public static void main(String[] args) {
+        // Инициализация.
         CalculatorModel model = new CalculatorModel(new CalculatorException());
         View ui = new View(new Scanner(System.in));
         Presenter presenter = new Presenter(model, ui);
-
+        
+        // Запуск калькулятора.
         presenter.start();
     }
 }
